ae07b43df025bae8622a0a424dbe03436b4f19f9
Erik Blomqvist <erikblomqvist1@gmail.com>
2024-10-08 11:50:37 +0300

    added ability to view project data to project.php

--- includes/class.project.php
+++ includes/class.project.php
@@ -16,23 +16,49 @@ class Project {
         $stmt_insertNewProject = $this->pdo->prepare('INSERT INTO table_projects (customer_id_fk, car_id_fk, defect_desc, work_desc, status_id_fk)
             VALUES 
             (:customerId, :carId, :defectDesc, :workDesc, 1)');
-            $stmt_insertNewProject->bindParam(':customerId', $customerId, PDO::PARAM_INT);
-            $stmt_insertNewProject->bindParam(':carId', $carId, PDO::PARAM_INT);
-            $stmt_insertNewProject->bindParam(':defectDesc', $defectDesc, PDO::PARAM_STR);
-            $stmt_insertNewProject->bindParam(':workDesc', $workDesc, PDO::PARAM_STR);
-            $stmt_insertNewProject->execute();
+        $stmt_insertNewProject->bindParam(':customerId', $customerId, PDO::PARAM_INT);
+        $stmt_insertNewProject->bindParam(':carId', $carId, PDO::PARAM_INT);
+        $stmt_insertNewProject->bindParam(':defectDesc', $defectDesc, PDO::PARAM_STR);
+        $stmt_insertNewProject->bindParam(':workDesc', $workDesc, PDO::PARAM_STR);
+        $stmt_insertNewProject->execute();
 
-            // Check if query returns any result
-            if($stmt_insertNewProject->rowCount() > 0) {
-                array_push($this->errorMessages, "Projektet finns redan ");
-                $this->errorState = 1;
-            }
+        // Check if query returns any result
+        if($stmt_insertNewProject->rowCount() > 0) {
+            array_push($this->errorMessages, "Projektet finns redan ");
+            $this->errorState = 1;
+        }
 
-            if ($this->errorState == 1) {
-                return $this->errorMessages;
-            } else {
-                return 1;    
-            }
+        if ($this->errorState == 1) {
+            return $this->errorMessages;
+        } else {
+            return 1;    
+        }
+    }
+
+    public function selectSingleProject($projectId) {
+        // Select data from single project in table_projects
+        $projectDataArray = $this->pdo->query("SELECT 
+            table_projects.*, 
+            table_customers.*, 
+            table_cars.*
+        FROM 
+            table_projects
+        JOIN 
+            table_customers ON table_projects.customer_id_fk = table_customers.customer_id
+        JOIN 
+            table_cars ON table_projects.car_id_fk = table_cars.car_id
+        WHERE 
+            table_projects.project_id = $projectId")->fetchAll();
+                return $projectDataArray;
+    }
+
+    public function selectProjectProducts($projectId) {
+        // Select all products linked to project in table_project_product
+        $projectProductsArray = $this->pdo->query("SELECT table_products.*
+            FROM table_products
+            JOIN table_project_product ON table_products.product_id = table_project_product.product_id_fk
+            WHERE table_project_product.project_id_fk = $projectId")->fetchAll();
+        return $projectProductsArray;
     }
 
 }
--- project.php
+++ project.php
@@ -1,6 +1,11 @@
 <?php
 include_once 'includes/header.php';
 
+$projectId = $_GET["project_id"];
+$projectDataArray = $project->selectSingleProject($projectId);
+$projectProductsArray = $project->selectProjectProducts($projectId);
+var_dump($projectDataArray);
+//var_dump($projectProductsArray);
 
 ?>
 
@@ -9,70 +14,62 @@ include_once 'includes/header.php';
 
 	<div class="mw-500 mx-auto">
 		<h1 class="my-5">Projekt</h1>
-		<a class="btn btn-primary">Redigera</a>
+
+		<a class="btn btn-primary mb-2">Redigera</a>
+
 		<h2 class="h4 my-3">Bil</h2>
 		<div class="row">
-			<div class="col">
-				<h3 class="h6">Märke+Modell</h3>
-			</div>
-			<div class="col">
-				<h3 class="h6">Registernummer</h3>
-			</div>
+			<p class="h6" id="car-data"><span id="car-brand"><?php echo $projectDataArray[0]['car_brand'] ?></span> <span id="car-model"><?php echo $projectDataArray[0]['car_model'] ?></span> <span class="ms-4" id="car-license"><?php echo $projectDataArray[0]['car_license'] ?></span></p>
 		</div>
-	<h2 class="h4 my-3">Kund</h2>
+
+		<h2 class="h4 mt-4 mb-3">Kund</h2>
+		<div class="row">
+			<p class="h6" id="customer-data">
+			<?php echo "{$projectDataArray[0]['customer_fname']} {$projectDataArray[0]['customer_lname']}" ?>
+				<?php echo "<span class='ms-4'> {$projectDataArray[0]['customer_phone']} </span>"?>
+				<?php echo "<span class='ms-4'> {$projectDataArray[0]['customer_email']} </span>"?>
+				<?php echo "<span class='ms-4'> {$projectDataArray[0]['customer_address']} {$projectDataArray[0]['customer_zip']} {$projectDataArray[0]['customer_area']} </span>"?>
+			</p>
+		</div>
+
+		<h2 class="h4 mt-4 mb-2">Om projektet</h2>
+
+		<label for="project-status" class="h5 my-3">Status</label>
+		<select id="project-status" name="project-status" class="form-select" aria-label="">
+			<option value="1">I kö</option>
+			<option value="2">Pågående</option>
+			<option value="3">Pausad</option>
+			<option value="3">Avbokad</option>
+			<option value="3">Klar för fakturering</option>
+		</select>
+
+		<h3 class="h5 my-3">Felbeskrivning</h3>
+		<p><?php echo "{$projectDataArray[0]['defect_desc']}"?></p>
+		<h3 class="h5 my-3">Arbetsbeskrivning</h3>
+		<p><?php echo "{$projectDataArray[0]['work_desc']}"?></p>
+
+		<div class="row">
+			<h3 class="h5 my-3">Produkter</h3>
+				<div class="col">
+					<ul class="list-group">
+						<li class="list-group-item">An item</li>
+						<li class="list-group-item">A second item</li>
+						<li class="list-group-item">A third item</li>
+						<li class="list-group-item">A fourth item</li>
+						<li class="list-group-item">And a fifth one</li>
+					</ul>
+					<a class="btn btn-success my-2">Lägg till</a>
+				</div>
+		</div>
+
 		<div class="row">
 			<div class="col">
-				<h3 class="h6">Namn</h3>
+				<p class="h5 my-3">Timmar:<span class="ms-4 fw-normal">34h 30min</span></p>
 			</div>
-			<div class="col">
-				<h3 class="h6">Telefonnummer</h3>
-			</div>
-			<div class="col">
-				<h3 class="h6">Epost</h3>
-			</div>
-			<div class="col mb-3">
-				<h3 class="h6">adress, Postnummer, Ort</h3>
-			</div>
-		</div>
-
-	<h2 class="h2 mt-3 mb-2">Om projektet</h2>
-
-	<h3 class="h4 my-3">Status</h3>
-	<select class="form-select" aria-label="">
-		<option value="1">I kö</option>
-		<option value="2">Pågående</option>
-		<option value="3">Pausad</option>
-		<option value="3">Avbokad</option>
-		<option value="3">Klar för fakturering</option>
-	</select>
-
-	<h3 class="h4 my-3">Felbeskrivning</h3>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum varius odio eget erat tincidunt, ut ullamcorper massa vestibulum. Vestibulum ex libero, luctus nec posuere vel, luctus vitae turpis. Etiam a turpis arcu. Maecenas et tellus nec massa imperdiet vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris quis tellus vitae massa suscipit volutpat a nec ipsum. Vestibulum ut pharetra neque, at tristique elit. Phasellus eleifend malesuada nulla, ac volutpat quam egestas sed. Pellentesque ac volutpat eros, vel pharetra velit.</p>
-	<h3 class="h4 my-3">Arbetsbeskrivning</h3>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum varius odio eget erat tincidunt, ut ullamcorper massa vestibulum. Vestibulum ex libero, luctus nec posuere vel, luctus vitae turpis. Etiam a turpis arcu. Maecenas et tellus nec massa imperdiet vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris quis tellus vitae massa suscipit volutpat a nec ipsum. Vestibulum ut pharetra neque, at tristique elit. Phasellus eleifend malesuada nulla, ac volutpat quam egestas sed. Pellentesque ac volutpat eros, vel pharetra velit.</p>
-
-	<div class="row">
-		<h3 class="h4 my-3">Produkter</h3>
-			<div class="col">
-				<ul class="list-group">
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">A second item</li>
-					<li class="list-group-item">A third item</li>
-					<li class="list-group-item">A fourth item</li>
-					<li class="list-group-item">And a fifth one</li>
-				</ul>
+			<div class="col text-center">
 				<a class="btn btn-success my-2">Lägg till</a>
 			</div>
-	</div>
-
-	<div class="row">
-		<div class="col">
-			<p class="h4 my-3">Timmar:<span class="ms-4 fw-normal">34h 30min</span></p>
 		</div>
-		<div class="col text-center">
-			<a class="btn btn-success my-2">Lägg till</a>
-		</div>
-	</div>
 
 	</div>
 </div>
