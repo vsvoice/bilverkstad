510edfd9e84b817abc565838b7be47c9a60f1cc9
Erik Blomqvist <erikblomqvist1@gmail.com>
2024-10-22 13:06:54 +0300

    added ability to add and edit project work hours as well as add products for projects

--- includes/class.project.php
+++ includes/class.project.php
@@ -61,6 +61,110 @@ class Project {
         return $projectProductsArray;
     }
 
+    public function insertNewProduct(string $prodName, string $prodPrice, string $prodNumber, int $projectId) {
+
+        $prodPrice = str_replace(',', '.', $prodPrice);
+        if (!is_numeric($prodPrice)) {
+            array_push($this->errorMessages, "Det angivna priset är inte ett giltigt tal ");
+            $this->errorState = 1;
+        }
+
+        $stmt_insertNewProduct = $this->pdo->prepare('INSERT INTO table_products (name, price, invoice_number)
+            VALUES 
+            (:prodName, :prodPrice, :prodNumber)');
+        $stmt_insertNewProduct->bindParam(':prodName', $prodName, PDO::PARAM_STR);
+        $stmt_insertNewProduct->bindParam(':prodPrice', $prodPrice, PDO::PARAM_STR);
+        $stmt_insertNewProduct->bindParam(':prodNumber', $prodNumber, PDO::PARAM_STR);
+        $stmt_insertNewProduct->execute();
+
+        $lastProductId = $this->pdo->lastInsertId();
+
+        $stmt_insertIntoProjectProduct = $this->pdo->prepare('INSERT INTO table_project_product (project_id_fk, product_id_fk)
+            VALUES 
+            (:projectId, :productId)');
+        $stmt_insertIntoProjectProduct->bindParam(':projectId', $projectId, PDO::PARAM_INT);
+        $stmt_insertIntoProjectProduct->bindParam(':productId', $lastProductId, PDO::PARAM_STR);
+        $stmt_insertIntoProjectProduct->execute();
+
+
+        // Check if query returns any result
+        if($stmt_insertIntoProjectProduct->rowCount() > 0) {
+            array_push($this->errorMessages, "Lyckades inte mata in i tabellen table_project_product ");
+            $this->errorState = 1;
+        }
+
+        if ($this->errorState == 1) {
+            return $this->errorMessages;
+        } else {
+            return 1;    
+        }
+
+    }
+
+    public function selectWorkingHours(string $date, int $userId, int $projectId) {
+
+        // SELECT hours of current user on selected date AS WELL AS user total hours on project from table_hours
+        $stmt_selectWorkingHours = $this->pdo->prepare('SELECT *, 
+            (SELECT SUM(h_amount) FROM table_hours WHERE u_id_fk = :uid_sub AND p_id_fk = :pid_sub) as total_hours 
+            FROM table_hours 
+            WHERE u_id_fk = :uid AND h_date = :hdate AND p_id_fk = :pid'
+        );
+        
+        // Bind parameters for the outer query
+        $stmt_selectWorkingHours->bindParam(':uid', $userId, PDO::PARAM_INT);
+        $stmt_selectWorkingHours->bindParam(':hdate', $date, PDO::PARAM_STR);
+        $stmt_selectWorkingHours->bindParam(':pid', $projectId, PDO::PARAM_INT);
+
+        // Bind parameters for the subquery
+        $stmt_selectWorkingHours->bindParam(':uid_sub', $userId, PDO::PARAM_INT);
+        $stmt_selectWorkingHours->bindParam(':pid_sub', $projectId, PDO::PARAM_INT);
+        $stmt_selectWorkingHours->execute();
+        
+        return $stmt_selectWorkingHours->fetch();
+    }
+
+    public function insertWorkingHours(int $projectId, int $userId, int $hours, string $date) {
+
+        if ($this->selectWorkingHours($date, $userId, $projectId) === false) 
+        {
+            $stmt_insertWorkingHours = $this->pdo->prepare('INSERT INTO table_hours (p_id_fk, u_id_fk, h_amount, h_date)
+            VALUES 
+            (:pid, :uid, :hamount, :hdate)');
+            $stmt_insertWorkingHours->bindParam(':pid', $projectId, PDO::PARAM_INT);
+            $stmt_insertWorkingHours->bindParam(':uid', $userId, PDO::PARAM_INT);
+            $stmt_insertWorkingHours->bindParam(':hamount', $hours, PDO::PARAM_INT);
+            $stmt_insertWorkingHours->bindParam(':hdate', $date, PDO::PARAM_STR);
+
+            if(!$stmt_insertWorkingHours->execute()) {
+                array_push($this->errorMessages, "Lyckades inte mata in arbetstiden ");
+                $this->errorState = 1;
+            }
+        }
+        else 
+        {
+            $stmt_editWorkingHours = $this->pdo->prepare('
+            UPDATE table_hours
+            SET h_amount = :hamount
+            WHERE p_id_fk = :pid AND u_id_fk = :uid AND h_date = :hdate');
+            $stmt_editWorkingHours->bindParam(':hamount', $hours, PDO::PARAM_INT);
+            $stmt_editWorkingHours->bindParam(':pid', $projectId, PDO::PARAM_INT);
+            $stmt_editWorkingHours->bindParam(':uid', $userId, PDO::PARAM_INT);
+            $stmt_editWorkingHours->bindParam(':hdate', $date, PDO::PARAM_STR);
+
+            if(!$stmt_editWorkingHours->execute()) {
+                array_push($this->errorMessages, "Lyckades inte uppdatera arbetstiden ");
+                $this->errorState = 1;
+            }
+    
+        }
+
+        if ($this->errorState == 1) {
+            return $this->errorMessages;
+        } else {
+            return 1;    
+        }
+    }
+
 }
 
 ?>
--- project.php
+++ project.php
@@ -1,11 +1,48 @@
 <?php
 include_once 'includes/header.php';
 
-$projectId = $_GET["project_id"];
+$projectId = (int)$_GET["project_id"];
+$currentDate = date("Y-m-d");
+
 $projectDataArray = $project->selectSingleProject($projectId);
 $projectProductsArray = $project->selectProjectProducts($projectId);
-var_dump($projectDataArray);
+$todaysWorkingHours = $project->selectWorkingHours($currentDate, $_SESSION['user_id'], $projectId);
+var_dump($currentDate);
+var_dump($projectId);
+var_dump($_SESSION['user_id']);
+var_dump($todaysWorkingHours);
+//var_dump($projectDataArray);
 //var_dump($projectProductsArray);
+
+if (isset($_POST['product-submit'])) {
+    $feedbackMessages = $project->insertNewProduct(
+		$user->cleanInput( $_POST['prod-name']), 
+		$user->cleanInput( $_POST['prod-price']), 
+		$user->cleanInput( $_POST['prod-number']),
+		$projectId
+	);
+
+	if($feedbackMessages !== 1) {
+		foreach($feedbackMessages as $message) {
+			echo $message;
+		}
+    }
+}
+
+if (isset($_POST['work-hours-submit'])) {
+    $feedbackMessages = $project->insertWorkingHours(
+		$projectId,
+		$_SESSION['user_id'],
+		$user->cleanInput( $_POST['work-hours']),
+		$user->cleanInput( $_POST['work-date'])
+	);
+
+	if($feedbackMessages !== 1) {
+		foreach($feedbackMessages as $message) {
+			echo $message;
+		}
+    }
+}
 
 ?>
 
@@ -32,8 +69,9 @@ var_dump($projectDataArray);
 			</p>
 		</div>
 
-		<h2 class="h4 mt-4 mb-2">Om projektet</h2>
+		<h2 class="h4 mt-5 mb-2 fw-bold">Om projektet</h2>
 
+		<!--TODO: Status Change Feature -->
 		<label for="project-status" class="h5 my-3">Status</label>
 		<select id="project-status" name="project-status" class="form-select" aria-label="">
 			<option value="1">I kö</option>
@@ -43,36 +81,139 @@ var_dump($projectDataArray);
 			<option value="3">Klar för fakturering</option>
 		</select>
 
-		<h3 class="h5 my-3">Felbeskrivning</h3>
+		<h3 class="h5 mt-4 mb-3">Felbeskrivning</h3>
 		<p><?php echo "{$projectDataArray[0]['defect_desc']}"?></p>
-		<h3 class="h5 my-3">Arbetsbeskrivning</h3>
+		<h3 class="h5 mt-4 mb-3">Arbetsbeskrivning</h3>
 		<p><?php echo "{$projectDataArray[0]['work_desc']}"?></p>
 
 		<div class="row">
 			<h3 class="h5 my-3">Produkter</h3>
 				<div class="col">
-					<ul class="list-group">
-						<li class="list-group-item">An item</li>
+					<!--<ul class="list-group">-->
+						<?php
+						echo "<ul class='list-group list-group-flush'>";
+
+						foreach ($projectProductsArray as $product) {
+							echo "<li class='list-group-item d-flex align-items-center'>"
+								 . $product['name'] 
+								 . " <span class='ms-3'>" 
+								 . number_format($product['price'], 2, ',', ' ') 
+								 . " €</span></li>";
+						}
+				
+						echo "</ul>";
+						?>
+						<!--<li class="list-group-item d-flex align-items-center">An item <i class="ms-auto fs-5 fa-regular fa-trash-can"></i></li>
 						<li class="list-group-item">A second item</li>
 						<li class="list-group-item">A third item</li>
 						<li class="list-group-item">A fourth item</li>
 						<li class="list-group-item">And a fifth one</li>
-					</ul>
-					<a class="btn btn-success my-2">Lägg till</a>
+					</ul>-->
+					<button type="button" class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#productModal">
+						Lägg till
+					</button>
 				</div>
 		</div>
 
 		<div class="row">
 			<div class="col">
-				<p class="h5 my-3">Timmar:<span class="ms-4 fw-normal">34h 30min</span></p>
+				<p class="h5 my-3">Timmar:<span class="ms-4 fw-normal"><?php echo $todaysWorkingHours['total_hours']; ?></span></p>
 			</div>
 			<div class="col text-center">
-				<a class="btn btn-success my-2">Lägg till</a>
+				<button type="button" class="btn btn-success my-2" data-bs-toggle="modal" data-bs-target="#workingHoursModal">
+					Lägg till
+				</button>
 			</div>
 		</div>
 
 	</div>
 </div>
+
+<div class="modal fade" id="productModal" tabindex="-1" aria-labelledby="productModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="productModalLabel">Lägg till produkt</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+
+			<form id="project-form" action="" method="post">
+				<div class="modal-body">
+
+					<label for="prod-name" class="form-label">Produktnamn*</label>
+					<input type="text" class="form-control" id="prod-name" name="prod-name" required><br>
+
+					<label for="prod-price" class="form-label">Pris*</label>
+					<input type="text" class="form-control" id="prod-price" name="prod-price" required><br>
+
+					<label for="prod-number" class="form-label">Produktnummer*</label>
+					<input type="text" class="form-control" id="prod-number" name="prod-number" required><br>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary me-auto" data-bs-dismiss="modal">Avbryt</button>
+					<input type="submit" name="product-submit" class="btn btn-primary" value="Lägg till">
+				</div>
+			</form>
+		
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="workingHoursModal" tabindex="-1" aria-labelledby="workingHoursModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="workingHoursModalLabel">Lägg till arbetstid</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+
+			<form id="work-hours-form" action="" method="post">
+				<div class="modal-body">
+
+					<label for="work-date" class="form-label">Datum</label>
+					<input type="date" class="form-control" id="work-date" name="work-date" value="<?php echo $currentDate; ?>" required><br>
+
+					<label for="work-hours" class="form-label">Antal timmar</label>
+					<input type="text" class="form-control" id="work-hours" name="work-hours" value="<?php echo $todaysWorkingHours['h_amount']; ?>" required><br>
+
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary me-auto" data-bs-dismiss="modal">Avbryt</button>
+					<input type="submit" name="work-hours-submit" class="btn btn-primary" value="Lägg till">
+				</div>
+			</form>
+		
+		</div>
+	</div>
+</div>
+
+<script>
+	const workDate = document.getElementById("work-date");
+	const workHours = document.getElementById("work-hours")
+
+	workDate.addEventListener("change", function() {
+		if (this.value.length == 0) {
+			workHours.value = "";
+			return;
+		} else {
+			var xmlhttp = new XMLHttpRequest();
+			xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				workHours.value = this.responseText;
+			}
+			};
+
+			const userId = '<?php echo $_SESSION["user_id"]; ?>';
+            const projectId = '<?php echo $projectId; ?>';
+            const selectedDate = this.value;
+
+			xmlhttp.open("GET", "ajax/select_work_hours_by_date.php?pid=" + projectId + "&uid=" + userId + "&date=" + selectedDate, true);
+			xmlhttp.send();
+		}
+    });
+</script>
 
 <?php
 include_once 'includes/footer.php';
